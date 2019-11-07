5efd495b1751dd7a1667dd77ef0cc115ac577d1a
Paulo Chaves <opaulochaves@gmail.com>
2019-11-07 09:34:53 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.disable("x-powered-by");
+
+app.use("/", (_req, res) => {
+  res.json({ text: "Hello, World!" });
+});
+
+module.exports = app;
---
+++ server/start.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 3000;
+
+const app = require("../server");
+
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", err => {
+    console.log("ERROR: ", err);
+  });
