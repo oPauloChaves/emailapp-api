d7b618ca1702114929deb443b50187a2ae611780
Paulo Chaves <opaulochaves@gmail.com>
2019-11-07 10:04:15 -0300

    config prettier and eslint

--- server/start.js
+++ server/start.js
@@ -1,11 +1,11 @@
 const PORT = process.env.PORT || 3000;
 
-const app = require("../server");
+const app = require('../server');
 
 app
   .listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   })
-  .on("error", err => {
-    console.log("ERROR: ", err);
+  .on('error', err => {
+    console.log('ERROR: ', err);
   });
