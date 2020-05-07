270151e211d5bd4651a2ce60627cf6f8d05b30e6
Paulo Chaves <opaulochaves@gmail.com>
2020-05-07 16:37:48 -0300

    update endpoint to fetch contacts with search

--- src/app.js
+++ src/app.js
@@ -8,11 +8,22 @@ const app = express();
 app.use(bodyParser.json());
 app.disable('x-powered-by');
 
-app.use('/api/users', async (_, res) => {
+app.get('/contacts', async (req, res) => {
   try {
-    const users = await db('users').select('id', 'email', 'name', 'image', 'phone', 'website');
+    const { q = '' } = req.query;
 
-    res.json(users);
+    console.log('[contacts][get] query contacts %j', { q });
+
+    // https://stackoverflow.com/questions/7005302/postgresql-how-to-make-case-insensitive-query#comment8370418_7005332
+    // specify varchar_pattern_ops if you want the index to work with LIKE 'xxx%' query,
+    // i.e. CREATE INDEX ix_groups_name ON groups (lower(name) varchar_pattern_ops)
+    const contacts = await db('users')
+      .where('name', 'ilike', `%${q}%`)
+      .select('id', 'email', 'name', 'image', 'phone', 'website');
+
+    console.log('[contacts][get] found %d contacts', contacts.length);
+
+    res.json(contacts);
   } catch (err) {
     res.status(500).json({ err: `ERROR: ${err}` });
   }
