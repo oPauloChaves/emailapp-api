672724fed82a4b2aea71a98125ab14abce493a75
Paulo Chaves <opaulochaves@gmail.com>
2020-05-07 12:00:32 -0300

    update to match new app needs

--- postgres-initdb.sh
+++ postgres-initdb.sh
@@ -5,12 +5,12 @@ psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE "thynances_test";
 EOSQL
 
-psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=thynances_dev <<-EOSQL
+psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=emailapp_dev <<-EOSQL
   CREATE EXTENSION "uuid-ossp";
   CREATE EXTENSION "hstore";
 EOSQL
 
-psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=thynances_test <<-EOSQL
+psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=emailapp_test <<-EOSQL
   CREATE EXTENSION "uuid-ossp";
   CREATE EXTENSION "hstore";
 EOSQL
--- src/migrations/20191107104705_init.js
+++ src/migrations/20191107104705_init.js
@@ -8,71 +8,8 @@ module.exports.up = async db => {
     table.string('password').notNullable();
     table.timestamps(false, true);
   });
-
-  await db.schema.createTable('accounts', table => {
-    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary();
-    table.string('name');
-    table.string('currency');
-  });
-
-  await db.schema.createTable('users_accounts', table => {
-    table.uuid('account_id').notNullable().references('id').inTable('accounts');
-    table.uuid('user_id').notNullable().references('id').inTable('users');
-    table.string('role');
-    table.primary(['account_id', 'user_id']);
-    table.timestamps(false, true);
-  });
-
-  await db.schema.createTable('categories', table => {
-    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary();
-    table.string('name').notNullable();
-    table.string('type').notNullable();
-    table.string('icon');
-    table.string('color');
-    table.boolean('deleted').notNullable().defaultTo(false);
-    table.uuid('account_id').notNullable().references('id').inTable('accounts');
-    table.uuid('user_id').notNullable().references('id').inTable('users');
-    table.timestamps(false, true);
-  });
-
-  await db.schema.createTable('finance_accounts', table => {
-    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary();
-    table.string('name').notNullable();
-    table.string('type').notNullable().defaultTo('checking');
-    table.decimal('balance', 8, 2)
-    table.string('icon');
-    table.string('color');
-    table.boolean('deleted').notNullable().defaultTo(false);
-    table.uuid('account_id').notNullable().references('id').inTable('accounts');
-    table.uuid('user_id').notNullable().references('id').inTable('users');
-    table.timestamps(false, true);
-  });
-
-  await db.schema.createTable('transactions', table => {
-    table.uuid('id').notNullable().defaultTo(db.raw('uuid_generate_v1mc()')).primary();
-    table.string('title');
-    table.string('type').notNullable();
-    table.timestamp('trade_date').defaultTo(db.fn.now());
-    table.decimal('value', 8, 2)
-    table.boolean('paid').notNullable().defaultTo(true);
-    table.string('note');
-    table.timestamp('remind_at');
-    table.integer('sequence').notNullable().defaultTo(0);
-    table.integer('total_sequence').notNullable().defaultTo(0);
-    table.uuid('category_id').notNullable().references('id').inTable('categories');
-    table.uuid('finance_account_id').notNullable().references('id').inTable('finance_accounts');
-    table.uuid('from_finance_account_id').references('id').inTable('finance_accounts');
-    table.uuid('account_id').notNullable().references('id').inTable('accounts');
-    table.uuid('user_id').notNullable().references('id').inTable('users');
-    table.timestamps(false, true);
-  });
 };
 
 module.exports.down = async db => {
-  await db.schema.dropTableIfExists('transactions');
-  await db.schema.dropTableIfExists('finance_accounts');
-  await db.schema.dropTableIfExists('categories');
-  await db.schema.dropTableIfExists('users_accounts');
-  await db.schema.dropTableIfExists('accounts');
   await db.schema.dropTableIfExists('users');
 };
